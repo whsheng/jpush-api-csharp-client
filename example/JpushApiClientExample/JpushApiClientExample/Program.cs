a35378e1e402da8ebecc97ad9b7280013117ed29
xinxin <xinxin@jpush.cn>
2013-12-12 14:27:56 +0800

    first c# commit

---
+++ example/JpushApiClientExample/JpushApiClientExample/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JpushApiClient;
+
+namespace JpushApiClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("************");
+            Console.WriteLine();
+
+            String result;
+            String master_secret = "570f9aadcffe791658dde66b";
+            String app_key = "7ebc243ae2b37128472b0875";
+            int sendno = 9;
+            JpushClient client = new JpushClient(app_key, master_secret, 0);
+            result = client.sendNotificationByTag("tag", sendno, "des", "tag notify title", "tag notify content", "android", "", "");
+
+            Console.Write("sendNotificationByTag************" + result);
+            Console.WriteLine();
+
+            
+
+              //
+              sendno++;
+             result = client.sendCustomMesByTag("tag", sendno, "des", "tag notify title", "tag notify content", "android", "", "");
+
+             Console.Write("sendCustomMesByTag************" + result);
+            Console.WriteLine();
+            
+              //
+              sendno++;
+             result = client.sendNotificationByAlias("alias", sendno, "des", "tag notify title", "tag notify content", "android", "", "");
+
+             Console.Write("sendNotificationByAlias************" + result);
+            Console.WriteLine();
+            
+              //
+              sendno++;
+             result = client.sendCustomMesByAlias("alias", sendno, "des", "tag notify title", "tag notify content", "android", "", "");
+
+             Console.Write("sendCustomMesByAlias************" + result);
+            Console.WriteLine();
+              //
+              //
+              sendno++;
+             result = client.sendNotificationByAppkey(sendno, "des", "tag notify title", "tag notify content", "android", "", "");
+
+            Console.Write("sendNotificationByTag************" + result);
+            Console.WriteLine();
+              //
+              //
+              sendno++;
+             result = client.sendCustomMesByAppkey(sendno, "des", "tag notify title", "tag notify content", "android", "", "");
+
+            Console.Write("sendNotificationByTag************" + result);
+            Console.WriteLine();
+
+              String msg_ids="929123086,1197558554";
+             result = client.getReceivedApi(msg_ids);
+
+             Console.Write("getReceivedApi************" + result);
+            Console.WriteLine();
+        }
+    
+    }
+}
